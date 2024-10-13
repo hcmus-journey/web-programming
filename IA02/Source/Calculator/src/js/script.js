1f6eb46b25fbb7567b25135f48d5133af20588b2
Tran Tien <trantien4868@gmail.com>
2024-10-14 02:00:36 +0700

    upload IA02

---
+++ IA02/Source/Calculator/src/js/script.js
@@ -0,0 +1,69 @@
+function calculate() {
+    const num1Input = document.getElementById('num1');
+    const num2Input = document.getElementById('num2');
+    const errorMessage = document.getElementById('error-message');
+    const operation = document.querySelector('input[name="operation"]:checked');
+    const alert = document.getElementById('alert');
+    // Clear previous error messages
+    errorMessage.innerHTML = '';
+    alert.style.visibility = 'hidden'
+
+    if (!checkValidInput(num1Input, num2Input, operation)) {
+        alert.style.visibility = 'visible'
+        return;
+    }
+
+    const num1 = parseFloat(num1Input.value);
+    const num2 = parseFloat(num2Input.value);
+
+    let result;
+    switch (operation.value) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            if (num2 === 0) {
+                errorMessage.innerHTML = 'Không thể chia cho 0';
+                alert.style.visibility = 'visible'
+                return;
+            }
+            result = num1 / num2;
+            break;
+    }
+    document.getElementById('result').value = result; // Display the result
+}
+
+function checkValidInput(num1Input, num2Input, operation) {
+    const errorMessage = document.getElementById('error-message');
+
+    if (!isDecimalNumber(num1Input.value)) {
+        errorMessage.innerHTML = 'Giá trị nhập ở ô <i>Số thứ nhất</i> không phải là số';
+        num1Input.focus();
+        return false;
+    }
+    if (!isDecimalNumber(num2Input.value)) {
+        errorMessage.innerHTML = 'Giá trị nhập ở ô <i>Số thứ hai</i> không phải là số';
+        num2Input.focus();
+        return false;
+    }
+    if (!operation) {
+        errorMessage.innerHTML = 'Vui lòng chọn phép toán';
+        return false;
+    }
+
+    return true;
+}
+
+function isDecimalNumber(num) {
+    // Use the match() method with a regular expression
+    const isDecimal = num.match(/^-?\d*\.?\d+$/);
+ 
+    // Return true if it's a valid decimal number, otherwise return false
+    return isDecimal !== null;
+}
