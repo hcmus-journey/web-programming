b324799e06c153be89b5bf99fe643979d8b82e02
thinant <nguynthinan@outlook.com>
2024-10-29 20:21:21 +0700

    update id, class; delete rates fields; create Product class and list of products

---
+++ GA03/Source/src/js/models/Product.js
@@ -0,0 +1,14 @@
+class Product {
+    constructor(imgLink, name, available, brand, category, sku, price, ogPrice, description) {
+        this.imgLink = imgLink;
+        this.name = name;
+        this.available = available;
+        this.brand = brand;
+        this.category = category;
+        this.sku = sku;
+        this.price = price;
+        this.ogPrice = ogPrice;
+        this.description = description;
+    }
+}
+
