058b4bf4c5643772f2cf9fdcf63abad8294f2a7f
thinant <nguynthinan@outlook.com>
2024-12-10 21:57:57 +0700

    update change profile feature

--- FinalProject/Source/routes/AdminRouter.js
+++ FinalProject/Source/routes/AdminRouter.js
@@ -5,4 +5,8 @@ const router = express.Router();
 
 router.get("/dashboard", adminController.showProfilePage);
 
+router.put("/dashboard", adminController.updateProfile);
+
+
+
 export default router;
